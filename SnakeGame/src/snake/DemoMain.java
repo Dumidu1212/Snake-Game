cc8d98e50f0f3383febf2cf97df5de8253079766
TL <TL@gmail.com>
2023-11-29 22:48:57 +0530

    jii

--- SnakeGame/src/snake/DemoMain.java
+++ SnakeGame/src/snake/DemoMain.java
@@ -4,9 +4,8 @@ public class DemoMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world !");
-		System.out.println("asdfas");
-		System.out.println("eewfewfd");
-		System.out.println("fyufgyu");
+
+		
 
 	}
 
